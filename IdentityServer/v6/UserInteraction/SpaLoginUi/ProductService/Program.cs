40e7d3b23538259a25d337869592761ce61d1975
zhengshouqiang <zsq@ilng.cc>
2023-11-28 16:21:20 +0800

    api乱码问题

--- IdentityServer/v6/UserInteraction/SpaLoginUi/ProductService/Program.cs
+++ IdentityServer/v6/UserInteraction/SpaLoginUi/ProductService/Program.cs
@@ -1,9 +1,17 @@
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);
+});
 
 builder.Services
     .AddAuthentication("Bearer")
